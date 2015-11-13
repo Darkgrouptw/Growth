329bb5f4a5d8f43bfd0b4095721bbeb51815ea7b
Graphics <darkgrouptw@gmail.com>
2015-11-13 16:03:09 +0800

    安裝 Devise 並加入 name

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy] 
+  
 	def index
     #flash[:notice] = "早安！你好！"
     #flash[:warning] = "這是 warning 訊息！"
