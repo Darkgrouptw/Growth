eca70099f9c37086049625aed40f8dd857540532
Graphics <darkgrouptw@gmail.com>
2015-11-19 14:49:48 +0800

    現在可以因為 討論版  因為不同人在用這個討論板 要求加入討論版  在退出討論版

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,6 +24,7 @@ class GroupsController < ApplicationController
     @group = current_user.groups.new(group_params)
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path, notice: '新增討論板成功'
     else 
       flash[:alert] = "Title欄位沒有填"
@@ -47,7 +48,34 @@ class GroupsController < ApplicationController
     @group.destroy
     redirect_to groups_path, alert: "討論板已刪除"
   end
+
+def join
+  @group = Group.find(params[:id])
+
+  if !current_user.is_member_of?(@group)
+    current_user.join!(@group)
+    flash[:notice] = "加入本討論版成功！"
+  else
+    flash[:warning] = "你已經是本討論版成員了！"
+  end
+
+  redirect_to group_path(@group)
 end
+
+def quit
+  @group = Group.find(params[:id])
+
+  if current_user.is_member_of?(@group)
+    current_user.quit!(@group)
+    flash[:alert] = "已退出本討論版！"
+  else
+    flash[:warning] = "你不是本討論版成員，怎麼退出 XD"
+  end
+
+  redirect_to group_path(@group)
+end
+end
+
 private
   
   def group_params 
